6baf5a2c654fef87f4501dcd2eb056c13ea5dc6f
nishpoojari <nishpoojary97@gmail.com>
2026-06-07 18:21:21 +0530

    Fix imports for components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Achievements from "./components/Achievements";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
+      <About />
+      <Education />   {/* if you want Education inside About, import it there */}
+      <Skills />
       <Projects />
+      <Achievements />
+      <Contact />
+      <Footer />
     </div>
   );
 }
