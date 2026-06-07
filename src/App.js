bf6734e317a1a7427e9ef9b9b6f4528162c1bb38
nishpoojari <nishpoojary97@gmail.com>
2026-06-07 18:14:28 +0530

    Added portfolio

--- src/App.js
+++ src/App.js
@@ -1,30 +1,13 @@
-import React, { useState } from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
-import About from "./components/About";
-import Skills from "./components/Skills";
 import Projects from "./components/Projects";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
-
-import "./App.css";
 
 function App() {
-  const [theme, setTheme] = useState("dark-theme");
-
-const toggleTheme = () => {
-  setTheme(theme === "dark-theme" ? "light-theme" : "dark-theme");
-};
-
   return (
-    <div className={theme}>
-      <Navbar toggleTheme={toggleTheme} />
+    <div className="App">
+      <Navbar />
       <Hero />
-      <About />
-      <Skills />
       <Projects />
-      <Contact />
-       <Footer /> 
     </div>
   );
 }
