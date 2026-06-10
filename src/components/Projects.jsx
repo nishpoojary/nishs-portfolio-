af7cfe675d1063deab85681fd9ffd171ea0cd785
nishpoojari <nishpoojary97@gmail.com>
2026-06-10 11:40:55 +0530

    Updated Project.jsx component

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -12,7 +12,7 @@ const projects = [
       "Developed a web-based art gallery system with Artist and User modules. The platform allows artists to upload and manage artworks, while users can view, comment on, and add artworks to their wishlist. ",
     tags: ["Developed a web-based art gallery system with Artist and User modules. The platform allows artists to upload and manage artworks, while users can view, comment on, and add artworks to their wishlist. "],
     image: connectImg,
-    github: "https://github.com/yourrepo/connect",
+    github: "https://github.com/nishpoojary/Virtual-art-gallery",
   },
     {
     title: "FocusFlow",
@@ -20,7 +20,7 @@ const projects = [
       "A functional clone of YouTube utilizing the Rapid API to fetch and display video content.",
     tags: ["FocusFlow is a smart productivity hub that helps you organize tasks, manage schedules, and stay on track with reminders and analytics. It brings clarity to your workflow, making it easier to focus and achieve your goals"],
     image: FocusFlowimg,
-    github: "https://github.com/yourrepo/youtube-clone",
+    github: "https://github.com/nishpoojary/FocusFlow-App",
   },
   {
     title: "AgroCare",
@@ -28,7 +28,7 @@ const projects = [
       "A functional clone of YouTube utilizing the Rapid API to fetch and display video content.",
     tags: ["AgroCare is a two-in-one agriculture platform that performs soil analysis and provides intelligent fertilizer recommendations based on soil and crop data. The application also features an integrated fertilizer marketplace, allowing farmers to directly purchase the recommended products for improved crop productivity"],
     image: youtubeImg,
-    github: "https://github.com/yourrepo/youtube-clone",
+    github: "https://github.com/nishpoojary/AgroCare",
   },
   // add more projects here...
 ];
